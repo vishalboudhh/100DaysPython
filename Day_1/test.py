4c02fcd2de17d66c486ce1723ebcc3c0d5f87027
vishal meshram <107854847+vishalboudhh@users.noreply.github.com>
2025-11-30 01:08:51 +0530

    Enhance test.py to implement a Band Name Generator, prompting for city and pet name inputs.

--- Day_1/test.py
+++ Day_1/test.py
@@ -1,2 +1,22 @@
-print("Hello")
-print("World")
+# Print Statement
+# print("Vishal \nMeshram")
+# print ( "Hello" + " Vishal")
+
+# Input
+# print("Hello " + input("Enter your name:")+"!")
+
+#Variables
+# name = input("Enter your name:")
+# print("Hello " + name)
+
+# username = "VishalBoudh"
+# length = len(username)
+# print(length)
+
+print("Welcome to the Band Name Generator.")
+city =input("Which city did you grow up in ?\n")
+print(city)
+pet = input("What is the name of your pet ?\n")
+print(pet)
+
+print("Your band name is " + city + " " + pet)
