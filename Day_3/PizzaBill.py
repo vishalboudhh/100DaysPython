19f5913404489cd329fa0e8dadc26da1f68cb444
vishal meshram <107854847+vishalboudhh@users.noreply.github.com>
2025-12-06 22:16:20 +0530

    Refactor PizzaBill.py to simplify size and topping input checks for case insensitivity

--- Day_3/PizzaBill.py
+++ Day_3/PizzaBill.py
@@ -6,24 +6,24 @@ extra_cheese = input("Do you want extra cheese ? Y or N:").lower()
 bill = 0
 
 # Size
-if size == "s":
+if size == "s" or size == "S":
     bill += 15
-elif size == "m":
+elif size == "m" or size == "M":
     bill += 20
-elif size == "l":
+elif size == "l" or size == "L":
     bill += 30
 else:
     print("Invalid size selected!")
 
 # Pepperoni
-if pepperoni == "y":
+if pepperoni == "y" or pepperoni == "Y":
     if size == "s":
         bill += 2
     else:
         bill += 3
 
 # Extra Cheese
-if extra_cheese == "y":
+if extra_cheese == "y" or extra_cheese == "Y":
     bill += 1
 
 print(f"Your final bill is : ${bill}")
