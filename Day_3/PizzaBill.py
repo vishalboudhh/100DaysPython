651d76ef2407ea26ebd6c0a8238b5ae988129d77
vishal meshram <107854847+vishalboudhh@users.noreply.github.com>
2025-12-06 22:07:06 +0530

    Add pizza billing system and rollercoaster height check logic

---
+++ Day_3/PizzaBill.py
@@ -0,0 +1,29 @@
+print("Welcome to python delivery!")
+size = input("What size pizza do you want ? S, M or L: ").lower()
+pepperoni = input("Do you want pepperoni on your pizza ? Y or N:").lower()
+extra_cheese = input("Do you want extra cheese ? Y or N:").lower()
+
+bill = 0
+
+# Size
+if size == "s":
+    bill += 15
+elif size == "m":
+    bill += 20
+elif size == "l":
+    bill += 30
+else:
+    print("Invalid size selected!")
+
+# Pepperoni
+if pepperoni == "y":
+    if size == "s":
+        bill += 2
+    else:
+        bill += 3
+
+# Extra Cheese
+if extra_cheese == "y":
+    bill += 1
+
+print(f"Your final bill is : ${bill}")
---
+++ Day_3/ifElse.py
@@ -0,0 +1,31 @@
+# water_level = int(input("Enter the water level:"))
+# if water_level > 80:
+#     print('Over Flow')
+# else :
+#     print("Continue")
+
+# print("Welcome to rollercoaster ride !")
+# height = int(input("What is your height in cm ?\n"))
+# if height >= 120:
+#   print("You can ride the rollercoaster")
+# elif height == 100:
+#   print("Your height is 100")
+# else:
+#   print("You can not ride")   
+
+
+# Nested elif
+# print("Welcome to the rollercoaster ride!")
+# height = int(input("What is your height in cm? "))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster")
+#     age = int(input("What is your age? "))
+#     if age < 18:
+#         print('Yor can are under age')
+#     elif age > 40:
+#         print("you are too old")
+#     else:
+#         print("Your age is perfect")
+# else:
+#     print("Sorry you can't ride")
